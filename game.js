3833ea0dc951b2b948064299f316f56e7f00184d
vonHupfburg <csabasteiber@gmail.com>
2019-04-09 12:53:26 +0200

    feat: add roll function
    The handInterface can now roll, at this time only on page load.

--- game.js
+++ game.js
@@ -46,6 +46,11 @@ class RollInterface extends SlottedInterface {
   constructor(htmlObject, header, locX, locY){
       super(htmlObject, header, locX, locY);
       this.rollButton = this.createRollButton();
+      this.rollChanceRare = 30;
+      this.rollChanceEpic = 15;
+      this.rollChanceLgnd = 2.5;
+      this.rollChanceCmmn = 100 - (this.rollChanceEpic + this.rollChanceRare + this.rollChanceLgnd); //%
+      this.roll();
   }
 
   createRollButton() {
@@ -69,7 +74,50 @@ class RollInterface extends SlottedInterface {
   }
 
   roll() {
+    // Reroll entire Available Buildings.
+    for (var indexSlots = 0; indexSlots < this.maxSlots; indexSlots++) {
+      var tempBuilding = this.getRandomBuildingByRarity(this.rollRarity());
+      this.slotArray[indexSlots].takeNewContent(tempBuilding);
+    }
+  }
 
+  rollRarity() {
+    var tempReal = Math.random() * 100;
+    if (tempReal < this.rollChanceLgnd) { // roll is within (0, 2.5)
+      return "lgnd"
+    } else if (tempReal >= this.rollChanceLgnd & (tempReal < (this.rollChanceLgnd + this.rollChanceEpic))) { // roll is within (2.5, 17.5)
+      return "epic"
+    } else if ((tempReal >= (this.rollChanceLgnd + this.rollChanceEpic)) & (tempReal < (this.rollChanceLgnd + this.rollChanceEpic + this.rollChanceRare))) { // roll is within (17.5, 47.5)
+      return "rare"
+    } else { // roll is within (47.5, 100)
+      return "cmmn"
+    }
+  }
+
+  getRandomInteger(minValue, maxValue) {
+    var tempInteger = minValue + Math.floor((maxValue - minValue) * Math.random())
+    return tempInteger
+  }
+
+  getRarityArray(whichRarity) {
+    var wantThisArray = [];
+    if (whichRarity === "cmmn") {
+      wantThisArray = rarityArrayCmmn;
+    } else if (whichRarity === "rare") {
+      wantThisArray = rarityArrayRare;
+    } else if (whichRarity === "epic") {
+      wantThisArray = rarityArrayEpic;
+    } else if (whichRarity === "lgnd") {
+      wantThisArray = rarityArrayLgnd;
+    }
+    return wantThisArray
+  }
+
+  getRandomBuildingByRarity(whichRarity) {
+    var rarityArray = this.getRarityArray(whichRarity);
+    var tempInteger = this.getRandomInteger(0, rarityArray.length);
+    var tempBuilding = rarityArray[tempInteger]
+    return tempBuilding;
   }
 }
 
@@ -80,13 +128,14 @@ class Slot {
     this.locX = locX;
     this.locY = locY;
     this.parentHtmlObject = parentHtmlObject; // The table to which this slot belongs.
+    this.infoBlocOject = this.createInfoBloc();
     this.htmlObject = this.createSlot(); // The slot object itself.
     this.slotContent = null;
     this.img = null; // May contain an image.
     this.reposition();
   }
 
-  createSlot(whichSlot) {
+  createSlot() {
     var tempSlot = document.createElement("div");
     this.parentHtmlObject.appendChild(tempSlot);
     tempSlot.className = "slot";
@@ -95,9 +144,47 @@ class Slot {
     return tempSlot;
   }
 
+  createInfoBloc() {
+    var tempParagraph = document.createElement("div");
+    var tempLineBreak = document.createElement("br")
+    this.parentHtmlObject.appendChild(tempParagraph);
+    tempParagraph.className = "infoBlocOject"
+    tempParagraph.textContent = ""
+    tempParagraph.position = "absolute";
+    tempParagraph.style.left = this.locX;
+    tempParagraph.style.top = parseInt(this.locY, 10) + 55 + "px";
+    return tempParagraph
+  }
+
   reposition() {
     this.htmlObject.style.left = this.locX + "px";
     this.htmlObject.style.top = this.locY + "px";
+  }
+
+  takeNewContent(whichBuilding) {
+    if (whichBuilding.imgLink !== "") {
+      this.renderImage(whichBuilding.imgLink);
+    } else {
+      this.htmlObject.textContent = whichBuilding.handle
+    }
+    this.infoBlocOject.textContent = this.updateInfoBloc(whichBuilding.rarity, whichBuilding.costCM)
+    console.log(whichBuilding.rarity);
+  }
+
+  updateInfoBloc(rarity, costCM) {
+    var tempInfoBloc
+    console.log(rarity);
+    if (rarity === "cmmn") {
+      tempInfoBloc = "Common";
+    } else if (rarity === "rare") {
+      tempInfoBloc = "Rare";
+    } else if (rarity === "epic") {
+      tempInfoBloc = "Epic";
+    } else if (rarity === "lgnd") {
+      tempInfoBloc = "Legendary"
+    }
+    tempInfoBloc = tempInfoBloc + " " + costCM + "CM";
+    return tempInfoBloc
   }
 
   renderImage(whichImage) {
@@ -117,6 +204,9 @@ class RollSlot extends Slot {
 
   buyThisBuilding() { // TODO: This is a planned feature of RollSlot.
   }
+
+
+
 }
 
 class HandSlot extends Slot {
@@ -134,22 +224,10 @@ class HandSlot extends Slot {
   }
 }
 
-// ROLL FUNCTIONALITY
-var rollInterface = new RollInterface(document.getElementById("htmlRollInterface"), "Available Buildings", 50, 100);
-var handInterface = new SlottedInterface(document.getElementById("htmlHandInterface"), "Placeable Buildings", 530, 100);
-
-// CM display
-var gameplayCM = 10000;
-var gameplayCMhtmlObject = document.getElementById("tempInterface");
-var commonArray = [];
-var rareArray = [];
-var epicArray = [];
-var legendaryArray = [];
-
-function changeCM(whichAmount) {
-  gameplayCM = gameplayCM + whichAmount;
-  gameplayCMhtmlObject.textContent = "CM is " + gameplayCM + ".";
-}
+var rarityArrayCmmn = [];
+var rarityArrayRare = [];
+var rarityArrayEpic = [];
+var rarityArrayLgnd = [];
 
 class Building {
   constructor(handle, rarity, costCM, imgLink) {
@@ -157,31 +235,78 @@ class Building {
     this.rarity = rarity;
     this.costCM = costCM;
     this.imgLink = imgLink;
+    this.rarityImgLink;
     this.pushToArray();
   }
 
   pushToArray(){
-    if (this.rarity === "common"){
-      commonArray.push(this);
+    if (this.rarity === "cmmn"){
+      rarityArrayCmmn.push(this);
     }
     if (this.rarity === "rare"){
-      rareArray.push(this);
+      rarityArrayRare.push(this);
     }
     if (this.rarity === "epic"){
-      epicArray.push(this);
+      rarityArrayEpic.push(this);
     }
-    if (this.rarity === "legendary"){
-      legendaryArray.push(this);
+    if (this.rarity === "lgnd"){ //legendary
+      rarityArrayLgnd.push(this);
     }
   }
 }
 
 // COMMON BUILDINGS :
-new Building("red_gatherer_1", "common", 100, "");
-new Building("blue_gatherer_1", "common", 100, "");
-console.log(commonArray);
+// Gathrers:
+new Building("ora_gat", "cmmn", 100, "");
+new Building("red_gat", "cmmn", 100, "");
+new Building("blu_gat", "rare", 200, "");
+new Building("gre_gat", "rare", 140, "");
+new Building("pur_gat", "rare", 200, "");
+// Houses:
+new Building("ora_hou", "cmmn", 100, "");
+new Building("red_hou", "epic", 300, "");
+new Building("blu_hou", "rare", 200, "");
+new Building("gre_hou", "cmmn",  70, "");
+new Building("pur_hou", "lgnd", 500, "");
+// Labs:
+new Building("ora_lab", "cmmn", 150, "");
+new Building("red_lab", "rare", 300, "");
+new Building("blu_lab", "lgnd", 600, "");
+new Building("gre_lab", "epic", 210, "");
+new Building("pur_lab", "cmmn", 150, "");
+// Powerplant:
+new Building("ora_pow", "cmmn", 125, "");
+new Building("red_pow", "rare", 250, "");
+new Building("blu_pow", "epic", 375, "");
+new Building("gre_pow", "lgnd", 350, "");
+new Building("pur_pow", "rare", 250, "");
+// Bar:
+new Building("ora_bar", "cmmn", 150, "");
+new Building("red_bar", "lgnd", 600, "");
+new Building("blu_bar", "cmmn", 150, "");
+new Building("gre_bar", "rare", 210, "");
+new Building("pur_bar", "epic", 450, "");
+
+console.log(rarityArrayCmmn);
+console.log(rarityArrayRare);
+console.log(rarityArrayEpic);
+console.log(rarityArrayLgnd);
+
+// ROLL FUNCTIONALITY
+var rollInterface = new RollInterface(document.getElementById("htmlRollInterface"), "Available Buildings", 50, 100);
+var handInterface = new SlottedInterface(document.getElementById("htmlHandInterface"), "Placeable Buildings", 530, 100);
+var gameplayCM = 10000;
+var gameplayCMhtmlObject = document.getElementById("tempInterface");
 
 
+function changeCM(whichAmount) {
+  gameplayCM = gameplayCM + whichAmount;
+  gameplayCMhtmlObject.textContent = "CM is " + gameplayCM + ".";
+}
+
+// =======================
+//          MISC
+// =======================
 // MISC: Creates a phantom grid. This will later be the actual grid when grid is merged with roll.
 var htmlGrid = document.getElementById("htmlGrid");
 document.getElementsByTagName("h1")[0].textContent = "Roll Functionality"
